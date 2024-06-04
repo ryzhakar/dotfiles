7ce11ad5ab8135c37835cb588634d126f96df453
Arthur Ryzhak <ryzhakar@gmail.com>
2024-06-04 19:19:16 +0300

    init: bring everything together

---
+++ .config/nvim/lua/arthur/plugins-setup.lua
@@ -0,0 +1,137 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
+
+-- add list of plugins to install
+return packer.startup(function(use)
+	-- packer can manage itself
+	use("wbthomason/packer.nvim")
+    use("folke/tokyonight.nvim")
+	use("christoomey/vim-tmux-navigator") -- tmux & split window navigatn
+	use("szw/vim-maximizer") -- maximizes and restores current window
+    
+
+    -- plenary metaplugin 
+    use("nvim-lua/plenary.nvim")
+
+    
+
+    -- commenting with gc
+	use("numToStr/Comment.nvim")
+
+    -- file explorer
+    use("nvim-tree/nvim-tree.lua")
+
+	-- vs-code like icons
+	use("kyazdani42/nvim-web-devicons")
+
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
+
+    -- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+   
+    -- managing & installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig") -- easily configure language servers
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+
+    -- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+    use({
+        "L3MON4D3/LuaSnip",
+        requires = {"rafamadriz/friendly-snippets"}
+    })
+
+    -- treesitter configuration
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+            ts_update()
+        end,
+    })
+
+    -- git integration
+    use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+    use {
+        "zbirenbaum/copilot.lua",
+        config = function()
+            require("copilot").setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+            })
+        end
+    }
+    use {
+        "zbirenbaum/copilot-cmp",
+        after = { "copilot.lua" },
+        config = function()
+            require("copilot_cmp").setup()
+        end
+    }
+    -- -- Text wrapping
+    -- use({
+    --     "andrewferrier/wrapping.nvim",
+    --     config = function()
+    --         require("wrapping").setup()
+    --     end,
+    -- })
+    -- Semi-working obsidian plugin
+    use {
+        "epwalsh/obsidian.nvim",
+        config = function()
+            require("obsidian").setup({
+                dir = "~/brain", -- replace this with the path to your vault
+                disable_frontmatter = true,
+                completion = {
+                  nvim_cmp = true, -- enable nvim-cmp
+                },
+            })
+            vim.keymap.set("n", "gf", function()
+              if require("obsidian").util.cursor_on_markdown_link() then
+                return "<cmd>ObsidianFollowLink<CR>"
+              else
+                return "gf"
+              end
+            end, { noremap = false, expr = true })
+        end
+    }
+
+    if packer_bootstrap then
+		require("packer").sync()
+	end
+end)
