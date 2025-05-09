1031ec9f7851b523496ce6883cafcce6653798b1
Arthur Ryzhak <ryzhakar@gmail.com>
2025-05-10 01:18:10 +0300

    fix(neovim): mason update breaking changes

--- nvim/.config/nvim/lua/arthur/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/arthur/plugins/lsp/mason.lua
@@ -1,13 +1,13 @@
 -- import mason plugin safely
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-	return
+    return
 end
 
 -- import mason-lspconfig plugin safely
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-	return
+    return
 end
 
 -- enable mason
@@ -16,19 +16,18 @@ mason.setup({
         "bacon",
         "bacon-ls",
     },
-	automatic_installation = true, -- not the same as ensure_installed
 })
 
+-- Configure mason-lspconfig - specify automatic_enable as false
 mason_lspconfig.setup({
-	-- list of servers for mason to install
-	ensure_installed = {
-		"dockerls",
-		"pyright",
-		"rust_analyzer",
-		"taplo",
-		-- "yamlls",
-	},
-	-- auto-install configured servers (with lspconfig)
-	automatic_installation = true, -- not the same as ensure_installed
+    -- list of servers for mason to install
+    ensure_installed = {
+        "dockerls",
+        "pyright",
+        "rust_analyzer",
+        "taplo",
+        -- "yamlls",
+    },
+    -- This is the key change:
+    automatic_enable = false,
 })
-
